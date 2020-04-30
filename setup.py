0b24bc7e7228806ee2fe25424573b74c12a349fa
Zbyšek Zapadlík <zbysekzapadlik@seznam.cz>
2020-04-30 03:26:25 -0400

    Enriched setup by additional info

--- setup.py
+++ setup.py
@@ -2,4 +2,7 @@ from setuptools import setup, find_packages
 setup(name='HTMpandaVis',
       version='0.1',
       packages = ["PandaVis/pandaComm"],
+      author="Zbysek Zapadlik from HTM community",
+      url="https://github.com/htm-community/HTMpandaVi",
+      python_requires='>=3.6',
       )
