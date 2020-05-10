80b84a34a836e22e0f99ced17894353922123447
Zbysekz <zbysekzapadlik@seznam.cz>
2020-05-10 18:27:36 +0200

    PandaComm version 0.2

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 setup(name='HTMpandaVis',
-      version='0.1',
+      version='0.2',
       packages = ["PandaVis/pandaComm"],
       author="Zbysek Zapadlik from HTM community",
-      url="https://github.com/htm-community/HTMpandaVi",
+      url="https://github.com/htm-community/HTMpandaVis",
       python_requires='>=3.6',
       )
