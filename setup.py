194fe460b790ea6f11c3bb945b192ee3f92035ae
Zbyšek Zapadlík <zbysek.zapadlik@kk-alpha.com>
2022-03-01 08:21:09 +0100

    corrected version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(name='HTMpandaVis',
-      version='0.1',
+      version='1.0',
       packages = ["pandaBaker"],
       author="Zbysek Zapadlik from HTM community",
       url="https://github.com/htm-community/HTMpandaVis",
